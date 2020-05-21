f8e00e3491260cdf2cce9b71d3aba1e31c0a80b0
lezcano001 <eliaslezcano001@gmail.com>
2020-05-21 19:15:07 -0400

    Mejora en el codigo del componente CotizacionCompraVenta

--- src/components/CotizacionCompraVenta/index.js
+++ src/components/CotizacionCompraVenta/index.js
@@ -47,79 +47,50 @@ export default class CotizacionCompraVenta extends Component{
     }
 
     render(){
-        var prueba = 8;
-        setInterval(()=>(
-            prueba = 2,
-            setTimeout(()=>(prueba = 8), 1000)
-        ), (7000))
-        var moneda1 = "";
-        var moneda2 = "";
-        var moneda3 = "";
-        var moneda4 = "";
-        var {compraVenta} = this.state;            
-        if(compraVenta.length > 0){
-            moneda1 = (
-                <motion.div initial={{x: -500, opacity: 0}} transition={{loop: Infinity, repeatDelay: 10}} animate={{x: 0, opacity: 1}}>
-                        <div className="containerCotizacion" key={`moneda${compraVenta[0].orden}`}>
-                            <img src={`img/${compraVenta[0].monImg}`} alt={`Imagen ${compraVenta[0].orden}`}/>
-                            <p className="cotizacion">{compraVenta[0].monISO}</p>
-                            <p className="cotizacion">{compraVenta[0].monISOnom}</p>
-                            <p className="cotizacion">{compraVenta[0].cpa}</p>
-                            <p className="cotizacion">{compraVenta[0].vta}</p>
-                        </div>
-                </motion.div>
-            )
-            if(compraVenta.length > 1){
-                moneda2 = (
-                    <motion.div initial={{x: -500, opacity: 0}} transition={{delay: 1, loop: Infinity, repeatDelay:10}} animate={{x: 0, opacity: 1}}>
-                        <div className="containerCotizacion" key={`moneda${compraVenta[1].orden}`}>
-                            <img src={`img/${compraVenta[1].monImg}`} alt={`Imagen ${compraVenta[1].orden}`}/>
-                            <p className="cotizacion">{compraVenta[1].monISO}</p>
-                            <p className="cotizacion">{compraVenta[1].monISOnom}</p>
-                            <p className="cotizacion">{compraVenta[1].cpa}</p>
-                            <p className="cotizacion">{compraVenta[1].vta}</p>
-                        </div>
-                    </motion.div>
-                )
+        var {compraVenta} = this.state;
+
+        const container = {
+            hidden: {
+                x: -500,
+                opacity: 0
+            },
+            visible: {
+                x: 0, 
+                opacity: 1,
+                transition: {
+                    staggerChildren: 5,
+                }
             }
-            if(compraVenta.length > 2){
-                moneda3 = (
-                    <motion.div initial={{x: -500, opacity: 0}} transition={{delay: 2, loop: Infinity, repeatDelay:10}} animate={{x: 0, opacity: 1}}>
-                        <div className="containerCotizacion" key={`moneda${compraVenta[2].orden}`}>
-                            <img src={`img/${compraVenta[2].monImg}`} alt={`Imagen ${compraVenta[2].orden}`}/>
-                            <p className="cotizacion">{compraVenta[2].monISO}</p>
-                            <p className="cotizacion">{compraVenta[2].monISOnom}</p>
-                            <p className="cotizacion">{compraVenta[2].cpa}</p>
-                            <p className="cotizacion">{compraVenta[2].vta}</p>
-                        </div>
-                    </motion.div>
-                )
+        };
+
+        const monedaitem = {
+            hidden: {x: -500, opacity: 0},
+            visible:{
+                x: 0,
+                opacity: 1
             }
-            if(compraVenta.length > 3){
-                moneda4 = (
-                    <motion.div initial={{x: -500, opacity: 0}} transition={{delay: 3, loop: Infinity, repeatDelay:10}} animate={{x: 0, opacity: 1}}>
-                        <div className="containerCotizacion" key={`moneda${compraVenta[3].orden}`}>
-                            <img src={`img/${compraVenta[3].monImg}`} alt={`Imagen ${compraVenta[3].orden}`}/>
-                            <p className="cotizacion">{compraVenta[3].monISO}</p>
-                            <p className="cotizacion">{compraVenta[3].monISOnom}</p>
-                            <p className="cotizacion">{compraVenta[3].cpa}</p>
-                            <p className="cotizacion">{compraVenta[3].vta}</p>
-                        </div>
-                    </motion.div>
-                )
-            }            
         }
+
         return(
             <div>
                 <div className="containerCotizacionCompraVenta">
                     <div className="containerTitle">
                         <p className="titleItemCompra title">Compra</p>
                         <p className="titleItemVenta title">Venta</p>
-                    </div>                                    
-                    {moneda1}
-                    {moneda2}
-                    {moneda3}
-                    {moneda4}
+                    </div>
+                    <motion.div variants={container} initial="hidden" animate="visible">
+                        {compraVenta.map(moneda => (
+                            <motion.div variants={monedaitem}>
+                                <div className="containerCotizacion" key={`moneda${moneda.orden}`}>
+                                    <img src={`img/${moneda.monImg}`} alt={`Imagen ${moneda.orden}`}/>
+                                    <p className="cotizacion">{moneda.monISO}</p>
+                                    <p className="cotizacion">{moneda.monISOnom}</p>
+                                    <p className="cotizacion">{moneda.cpa}</p>
+                                    <p className="cotizacion">{moneda.vta}</p>
+                                </div>
+                            </motion.div>
+                        ))}
+                    </motion.div>
                 </div>
             </div>
         );
